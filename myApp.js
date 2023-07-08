de49945e75aa7eb23bcecd41241a07a4463f9013
Varu <vardaan.agarwal.98@gmail.com>
2023-07-08 19:17:22 +0530

    add : mongodb URI

--- myApp.js
+++ myApp.js
@@ -1,5 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 let Person;
 
