fc6b702b9880538f16ce1c37443dc602abe6a784
Varu <vardaan.agarwal.98@gmail.com>
2023-07-08 20:13:34 +0530

    add : person schema

--- myApp.js
+++ myApp.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 
 console.log(process.env.MONGO_URI);
 
+const personSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -15,7 +24,7 @@ mongoose
     console.log(err);
   });
 
-let Person;
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
