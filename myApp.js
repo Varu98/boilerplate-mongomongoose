6a455bd3a74e995f9e46702829c3973fb193f483
Varu <vardaan.agarwal.98@gmail.com>
2023-07-10 17:32:15 +0530

    add : remove many.

--- myApp.js
+++ myApp.js
@@ -100,10 +100,12 @@ const removeById = function (personId, done) {
   });
 };
 
-const removeManyPeople = (done) => {
+const removeManyPeople = function (done) {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, function (err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  });
 };
 
 const queryChain = (done) => {
